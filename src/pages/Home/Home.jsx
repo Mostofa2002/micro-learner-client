ef20384ab623e96e7191e92786b904e470a42bba
Mostofa2002 <taj.2020x@gmail.com>
2023-11-24 17:49:02 +0600

    node

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import Partners from "./Partners/Partners";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Partners />
     </div>
   );
 };
---
+++ src/pages/Home/Partners/Partners.jsx
@@ -0,0 +1,13 @@
+const Partners = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
+        <div>
+          <h1>taj</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
